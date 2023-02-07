a3f74100359304263cd44ef70a265a6abc81106d
unknown <felipecarro02@gmail.com>
2023-02-07 09:17:38 -0300

    Tipos e modos de operadores

---
+++ operadores.js
@@ -0,0 +1,11 @@
+    //operador + operação de adição 
+    //operador - operação de subtração
+    //operador * operação multiplacação
+    //operação / operação divisão
+    //operação = atribuição de valor
+    //operação == só compara valores
+    //operação === comparação de valor e tipo de dados
+
+    //operação ||: Operador “ou”, retorna true caso uma condição seja válida;
+    //operação &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+    //operação != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
