5948b5fdfb4d46c502f48979190ea8425ce29b4f
unknown <felipecarro02@gmail.com>
2023-02-07 09:32:34 -0300

    Declaração atualizada

--- declaração.js
+++ declaração.js
@@ -1,2 +1,7 @@
+const nome = "Felipe";
+const idade = 20;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
 const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
 console.log(pedido)
