dacb3735ccda21e1fb5f2f4ba9458837146aa657
unknown <felipecarro02@gmail.com>
2023-02-09 08:20:36 -0300

    Utilização de Comando Push

---
+++ Arrays/Push.js
@@ -0,0 +1,22 @@
+//ADICIONANDO PUSH
+
+//const notas = [10, 6, 8];
+//notas.push(7);
+//const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+//console.log(media);
+
+// VERIFICAÇÃO DE PUSH
+
+//const notas = [10, 6, 8];
+//notas.push(7);
+//console.log(notas);
+//const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+//console.log(media);
+
+//UTILIZANDO PUSH
+
+const notas = [10, 6, 8];
+notas.push(7);
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media);
+
